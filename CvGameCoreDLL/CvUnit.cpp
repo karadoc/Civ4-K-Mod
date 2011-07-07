50c23e46e5eaba016bfbd9bd943dfef44dc73048
karadoc <karadoc@gmail.com>
2011-07-08 02:25:59 +1000

    The immediate experience boost from Great Generals is now increased by 10% for each unit in the stack, up to a maximum of +100%.

--- CvGameCoreDLL/CvUnit.cpp
+++ CvGameCoreDLL/CvUnit.cpp
@@ -7086,7 +7086,9 @@ bool CvUnit::giveExperience()
 
 int CvUnit::getStackExperienceToGive(int iNumUnits) const
 {
-	return (m_pUnitInfo->getLeaderExperience() * (100 + std::min(50, (iNumUnits - 1) * GC.getDefineINT("WARLORD_EXTRA_EXPERIENCE_PER_UNIT_PERCENT")))) / 100;
+	//return (m_pUnitInfo->getLeaderExperience() * (100 + std::min(50, (iNumUnits - 1) * GC.getDefineINT("WARLORD_EXTRA_EXPERIENCE_PER_UNIT_PERCENT")))) / 100;
+	// K-Mod, +50% is too low as a maximum. I think 10 _units_ is more reasonable.
+	return (m_pUnitInfo->getLeaderExperience() * (100 + std::min(10, (iNumUnits - 1)) * GC.getDefineINT("WARLORD_EXTRA_EXPERIENCE_PER_UNIT_PERCENT"))) / 100;
 }
 
 int CvUnit::upgradePrice(UnitTypes eUnit) const
