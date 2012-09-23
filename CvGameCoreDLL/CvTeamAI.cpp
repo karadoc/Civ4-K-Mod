d4f66f3c9ef4997e9825d51f0f35b35a02bb839c
karadoc <karadoc@gmail.com>
2012-09-23 22:51:36 +1000

    The trade value of techs is now independant of team size.
    Usually, the trade value of a tech is proportional to its cost. The cost
    of techs is proportional to the number of civs on the team; and I think
    that including this team-size multiplier for trade was unfair and
    counter-intuitive.

--- CvGameCoreDLL/CvTeamAI.cpp
+++ CvGameCoreDLL/CvTeamAI.cpp
@@ -2241,8 +2241,8 @@ int CvTeamAI::AI_techTradeVal(TechTypes eTech, TeamTypes eTeam) const
 
 	iValue += (((iCost / 2) * (iPossibleKnownCount - iKnownCount)) / iPossibleKnownCount);
 	*/
-	// K-Mod
-	int iValue = (150 + AI_knownTechValModifier(eTech)) * std::max(0, (getResearchCost(eTech) - getResearchProgress(eTech))) / 100;
+	// K-Mod. Standardized the modifier for # of teams with the tech; and removed the effect of team size.
+	int iValue = (150 + AI_knownTechValModifier(eTech)) * std::max(0, (getResearchCost(eTech) - getResearchProgress(eTech))) / (100 * getNumMembers());
 	// K-Mod end
 
 	iValue *= std::max(0, (GC.getTechInfo(eTech).getAITradeModifier() + 100));
