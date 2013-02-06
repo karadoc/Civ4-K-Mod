karadoc/Civ4-K-Mod

commit 61167cfe7b758240039e5c96ae3600a837bf7385

Reduced the effect of pre-existing progress in AI_chooseBuilding.
Previously any pre-existing progress on the chosen building would
effectively boost the probability up to 100%. Now, the probability is
increased by progress / total cost.

Also, cleared out some bbai comments and clarified what was actually
added by bbai in a couple of places.

CvGameCoreDLL/CvCityAI.cpp
@@ -8396,11 +8396,6 @@ void CvCityAI::AI_doEmphasize()
 	}
 }
 
-/************************************************************************************************/
-/* BETTER_BTS_AI_MOD                      01/09/10                                jdog5000      */
-/*                                                                                              */
-/* City AI                                                                                      */
-/************************************************************************************************/
 bool CvCityAI::AI_chooseUnit(UnitAITypes eUnitAI, int iOdds)
 {
 	UnitTypes eBestUnit;
@@ -8688,9 +8683,16 @@ bool CvCityAI::AI_chooseBuilding(int iFocusFlags, int iMaxTurns, int iMinThresho
 
 	if (eBestBuilding != NO_BUILDING)
 	{
+		/* bbai code
 		if( iOdds < 0 || 
 			getBuildingProduction(eBestBuilding) > 0 ||
-			GC.getGameINLINE().getSorenRandNum(100,"City AI choose building") < iOdds )
+			GC.getGameINLINE().getSorenRandNum(100,"City AI choose building") < iOdds ) */
+		// K-Mod
+		int iRand;
+		if (iOdds < 0 ||
+			(iRand = GC.getGameINLINE().getSorenRandNum(100, "City AI choose building") < iOdds) ||
+			iRand < iOdds + 100*getBuildingProduction(eBestBuilding)/std::max(1, getProductionNeeded(eBestBuilding)))
+		// K-Mod end
 		{
 			pushOrder(ORDER_CONSTRUCT, eBestBuilding);
 			return true;
@@ -8699,10 +8701,6 @@ bool CvCityAI::AI_chooseBuilding(int iFocusFlags, int iMaxTurns, int iMinThresho
 
 	return false;
 }
-/************************************************************************************************/
-/* BETTER_BTS_AI_MOD                       END                                                  */
-/************************************************************************************************/
-
 
 bool CvCityAI::AI_chooseProject()
 {

CvGameCoreDLL/CvCityAI.h
@@ -189,22 +189,14 @@ protected:
 	bool AI_doPanic();
 	int AI_calculateCulturePressure(bool bGreatWork = false) const;
 
-/************************************************************************************************/
-/* BETTER_BTS_AI_MOD                      01/09/10                                jdog5000      */
-/*                                                                                              */
-/* City AI                                                                                      */
-/************************************************************************************************/
-	bool AI_chooseUnit(UnitAITypes eUnitAI = NO_UNITAI, int iOdds = -1);
+	bool AI_chooseUnit(UnitAITypes eUnitAI = NO_UNITAI, int iOdds = -1); // bbai added iOdds
 	bool AI_chooseUnit(UnitTypes eUnit, UnitAITypes eUnitAI);
 	
 	bool AI_chooseDefender();
-	bool AI_chooseLeastRepresentedUnit(UnitTypeWeightArray &allowedTypes, int iOdds = -1);
-	bool AI_chooseBuilding(int iFocusFlags = 0, int iMaxTurns = MAX_INT, int iMinThreshold = 0, int iOdds = -1);
+	bool AI_chooseLeastRepresentedUnit(UnitTypeWeightArray &allowedTypes, int iOdds = -1); // bbai added iOdds
+	bool AI_chooseBuilding(int iFocusFlags = 0, int iMaxTurns = MAX_INT, int iMinThreshold = 0, int iOdds = -1); // bbai added iOdds.
 	bool AI_chooseProject();
 	bool AI_chooseProcess(CommerceTypes eCommerceType = NO_COMMERCE);
-/************************************************************************************************/
-/* BETTER_BTS_AI_MOD                       END                                                  */
-/************************************************************************************************/
 
 	bool AI_bestSpreadUnit(bool bMissionary, bool bExecutive, int iBaseChance, UnitTypes* eBestSpreadUnit, int* iBestSpreadUnitValue);
 	bool AI_addBestCitizen(bool bWorkers, bool bSpecialists, int* piBestPlot = NULL, SpecialistTypes* peBestSpecialist = NULL);

commit 37cf538a84b3b96f1a57e33ccdbb0e3fa84fd469

Fixed a bug which broke CvCityAI::AI_chooseBuilding completely.
There was an order of precidence problem which was causing this function
to essentially choose the building for any non-zero odds.

This bug was introduced in 61167cfe7b75824003, when I tried to "Reduced
the effect of pre-existing progress in AI_chooseBuilding." That's a long
time ago for such a serious bug. I'm surprised I'm only noticing it
now...

CvGameCoreDLL/CvCityAI.cpp
@@ -8463,7 +8463,7 @@ bool CvCityAI::AI_chooseBuilding(int iFocusFlags, int iMaxTurns, int iMinThresho
 		// K-Mod
 		int iRand;
 		if (iOdds < 0 ||
-			(iRand = GC.getGameINLINE().getSorenRandNum(100, "City AI choose building") < iOdds) ||
+			(iRand = GC.getGameINLINE().getSorenRandNum(100, "City AI choose building")) < iOdds ||
 			iRand < iOdds + 100*getBuildingProduction(eBestBuilding)/std::max(1, getProductionNeeded(eBestBuilding)))
 		// K-Mod end
 		{