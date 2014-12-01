71988831326f9e0ad11fcf0c835821c322caa97b
karadoc <karadoc@gmail.com>
2014-12-01 12:18:50 +1100

    Increased tendency for settler and missionary boats to wait in cities.
    Units are more likely to load into boats if the boats are waiting in a
    city - and so to help the AI spread corporations, I've added a new
    condition for using missionary boats for exploring.
    
    The boats will now only explore if there aren't alrady enough explorers
    in the area.

--- CvGameCoreDLL/CvUnitAI.cpp
+++ CvGameCoreDLL/CvUnitAI.cpp
@@ -8451,7 +8451,9 @@ void CvUnitAI::AI_settlerSeaMove()
 /* BETTER_BTS_AI_MOD                       END                                                  */
 /************************************************************************************************/	
 	
-	if ((GC.getGame().getGameTurn() - getGameTurnCreated()) < 8)
+	//if ((GC.getGame().getGameTurn() - getGameTurnCreated()) < 8)
+	if (GC.getGame().getGameTurn() - getGameTurnCreated() < 8 && plot()->waterArea() &&
+		kOwner.AI_areaMissionAIs(plot()->waterArea(), MISSIONAI_EXPLORE, getGroup()) < kOwner.AI_neededExplorers(plot()->waterArea())) // K-Mod
 	{
 		if ((plot()->getPlotCity() == NULL) || kOwner.AI_totalAreaUnitAIs(plot()->area(), UNITAI_SETTLE) == 0)
 		{
@@ -8683,9 +8685,10 @@ void CvUnitAI::AI_missionarySeaMove()
 /* BETTER_BTS_AI_MOD                       END                                                  */
 /************************************************************************************************/	
 	
-	if (AI_explore())
+	if (plot()->waterArea() && kOwner.AI_areaMissionAIs(plot()->waterArea(), MISSIONAI_EXPLORE, getGroup()) < kOwner.AI_neededExplorers(plot()->waterArea())) // K-Mod
 	{
-		return;
+		if (AI_explore())
+			return;
 	}
 
 	if (AI_retreatToCity(true))
