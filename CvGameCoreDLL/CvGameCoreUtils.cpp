2883449e474c9cd1b95a5f1d5120a91a6ccdfe66
karadoc <karadoc@gmail.com>
2017-10-15 12:36:08 +1100

    Fixed estimateCollateralWeight with non-combat units.

--- CvGameCoreDLL/CvGameCoreUtils.cpp
+++ CvGameCoreDLL/CvGameCoreUtils.cpp
@@ -995,7 +995,7 @@ int estimateCollateralWeight(const CvPlot* pPlot, TeamTypes eAttackTeam, TeamTyp
 			// Or we could check all types... But the reality is, there are always going to be mods and fringe cases where
 			// the esitmate is inaccurate. And currently in K-Mod, all instances of immunity are to the units own type anyway.
 			// Whichever way we do the estimate, cho-ku-nu is going to mess it up anyway. (Unless I change the game mechanics.)
-			if (pLoopUnit->getUnitInfo().getUnitCombatCollateralImmune(pLoopUnit->getUnitCombatType()))
+			if (pLoopUnit->getUnitCombatType() != NO_UNITCOMBAT && pLoopUnit->getUnitInfo().getUnitCombatCollateralImmune(pLoopUnit->getUnitCombatType()))
 				iResistanceSum += 100;
 			else
 				iResistanceSum += pLoopUnit->getCollateralDamageProtection();
